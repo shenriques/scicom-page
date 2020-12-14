ada6da2b1d7ab1ba6de7c0b14018fceeb5b29577
Shani Henriques-Fraser <Shani.Henriques-Fraser@BuroHappold.com>
2020-12-14 15:29:59 +0000

    Clean up repeated code using a set index function

--- js/app.js
+++ js/app.js
@@ -10,15 +10,20 @@ const circleParent = document.querySelector('.controls ul');
 
 var contentIndex = 0;
 
+function setContentIdx() {
+    // if circle already selected, remove transparent class and go back to black
+    document.querySelector('.controls .selected').classList.remove('selected');
+    // go to selected page
+    slider.style.transform = 'translate(' + (contentIndex) * -25 + '%)';
+}
+
 // scrolling with carousel indicators
 document.querySelectorAll('.controls li').forEach(function(circle, index) {
     circle.addEventListener('click', function() {
         // keep track of page index changes from the arrows
         contentIndex = index;
-        // colour only the circle of the page in view
-        document.querySelector('.controls .selected').classList.remove('selected');
+        setContentIdx();
         circle.classList.add('selected');
-        slider.style.transform = 'translate(' + (contentIndex) * -25 + '%)';
     });
 });
 
@@ -26,17 +31,14 @@ document.querySelectorAll('.controls li').forEach(function(circle, index) {
 rightArrow.addEventListener('click', function() {
     // scroll right unless the content slider you're on is 3 (end of slider)
     contentIndex = (contentIndex < 3) ? contentIndex + 1 : 3;
-    // remove the circle colouring the previously viewed page
-    document.querySelector('.controls .selected').classList.remove('selected');
+    setContentIdx();
     // add circle colouring to new current page
     circleParent.children[contentIndex].classList.add('selected');
-    slider.style.transform = 'translate(' + (contentIndex) * -25 + '%)';
 });
 
 leftArrow.addEventListener('click', function() {
     // scroll left unless the content slider you're on is 0 (beginning of slider)
     contentIndex = (contentIndex > 0) ? contentIndex - 1 : 0;
-    document.querySelector('.controls .selected').classList.remove('selected');
+    setContentIdx();
     circleParent.children[contentIndex].classList.add('selected');
-    slider.style.transform = 'translate(' + (contentIndex) * -25 + '%)';
 });
